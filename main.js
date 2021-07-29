2c03da91dc8f81a00b87da2fb5422b96b393393d
Couapy <mael@marchand.cloud>
2021-07-29 09:18:33 +0200

    ci(npm): add npm workflow

---
+++ main.js
@@ -0,0 +1,14 @@
+import EmptyConfigFile from './src/errors/EmptyConfigFile'
+import EmptyMethods from './src/errors/EmptyMethods'
+import EmptyRoutes from './src/errors/EmptyRoutes'
+import FileNotFound from './src/errors/FileNotFound'
+import InvalidArgument from './src/errors/InvalidArgument'
+import InvalidRouteElement from './src/errors/InvalidRouteElement'
+import ModuleNotFound from './src/errors/ModuleNotFound'
+
+export { EmptyConfigFile, EmptyMethods, EmptyRoutes, FileNotFound, InvalidArgument, InvalidRouteElement, ModuleNotFound }
+
+import Parser from './src/Parser'
+import Validation from './src/Validation'
+
+export { Parser, Validation }
